c8f3b3cae221ac359db852b454806c4f66f3b90a
Wonfee <dasifly@gmail.com>
2015-12-26 14:50:44 +0800

    add non-block request support to simple http server.

--- requestHandler.js
+++ requestHandler.js
@@ -1,9 +1,24 @@
-function start(){
+var exec = require("child_process").exec;
+
+function start(response){
     console.log("Request handler 'start' was called.");
+    var content = "empty";
+
+    exec("find /",
+        { timeout: 10000, maxBuffer: 20000*1024},
+        function(error, stdout, stderr){
+                content = stdout;
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write(content);
+                response.end();
+    });
 }
 
-function upload(){
+function upload(response){
     console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
