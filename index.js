108444987dd652fd29eb819d3b6755b0725a4d82
Wonfee <dasifly@gmail.com>
2015-12-29 14:26:21 +0800

    support picture file upload and display.

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ var handle = {};
 handle["/"] = requestHandler.start;
 handle["/start"] = requestHandler.start;
 handle["/upload"] = requestHandler.upload;
+handle["/show"] = requestHandler.show;
 
 server.start(router.route, handle);
